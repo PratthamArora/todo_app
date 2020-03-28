f20cb201d66cc35264ce1353d0b4b8b0fe682744
PratthamArora <arora.prattham@gmail.com>
2020-03-28 17:47:18 +0530

    added chekbox logic

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,14 +1,46 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('Task 1'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (bool value) {},
+      title: Text(
+        'Task 1',
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
       ),
+      trailing: TaskCheckBox(
+        checkboxState: isChecked,
+        toggleCheckBox: (bool checkboxState) {
+          setState(() {
+            isChecked = checkboxState;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  final bool checkboxState;
+  final Function toggleCheckBox;
+
+  TaskCheckBox({this.checkboxState, this.toggleCheckBox});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkboxState,
+      onChanged: toggleCheckBox,
     );
   }
 }
